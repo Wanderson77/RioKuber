399f12dde448a2b047c35aa1287c7a4e55498291
Wanderson77 <wanderson.aldo@gmail.com>
2023-04-24 10:52:34 -0300

    RN for UI Core Components

--- src/components/Text/style.js
+++ src/components/Text/style.js
@@ -5,5 +5,21 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  }
+  },
+  text:{
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  likeButton:{
+    height: '50px',
+    width: '50px',
+  },
+  mapView:{
+    position: 'absolute',
+    top: '0',
+    bottom: '0',
+    left: '0',
+    right: '0',
+  },
 })
